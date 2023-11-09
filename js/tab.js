9d4ea5957cb925dbeeb1042784904526211e7fe3
AnderGI02 <ander.gutierrez.ibarguen@zabalburu.org>
2023-11-09 08:10:44 +0100

    Tab Pane js funciton added

--- js/tab.js
+++ js/tab.js
@@ -1,16 +1,21 @@
 const tabButtons = [...document.querySelectorAll("button.tabBtn")];
 tabButtons.forEach((btn, index, list) => {
   btn.addEventListener("click", (event) => {
+    // Que solo ocurra en el btn que no esta activo
     if (!btn.classList.contains("active")) {
-      const nextBtn = index === 0 ? 0 : 1;
+      // El indice del btn opuest
+      const nextBtn = index === 0 ? 1 : 0;
+      // Del que esta remueve la clase
       list[nextBtn].classList.remove("active");
+      // añadesela al btn sobre el que se está clickando
       btn.classList.add("active");
-      const controls = event.target.attributes["aria-controls"].value;
+
+      const controls = btn.attributes["aria-controls"].value;
       const elementDiv = document.getElementById(controls);
-      const nextId = elementDiv === "home" ? "profile" : "home";
+      const nextId = elementDiv.id === "home" ? "profile" : "home";
       if (!elementDiv.classList.contains(...["show", "active"])) {
         document.getElementById(nextId).classList.remove(...["show", "active"]);
-        document.getElementById(nextId).replaceChildren();
+        // document.getElementById(nextId).replaceChildren();
         elementDiv.classList.add(...["show", "active"]);
       }
     }
