cf6a1640072081e1e00dbc7a802293e7fff4dc11
AnderGI02 <ander.gutierrez.ibarguen@zabalburu.org>
2023-11-10 09:44:49 +0100

    Support tab added

--- js/tab.js
+++ js/tab.js
@@ -1,4 +1,12 @@
 const tabButtons = [...document.querySelectorAll("button.tabBtn")];
+const borderClasses = [
+  "border",
+  "border-left-0",
+  "border-right-0",
+  "border-top-0",
+  "border-bottom-5",
+  "border-dark",
+];
 tabButtons.forEach((btn, index, list) => {
   btn.addEventListener("click", (event) => {
     // Que solo ocurra en el btn que no esta activo
@@ -6,9 +14,9 @@ tabButtons.forEach((btn, index, list) => {
       // El indice del btn opuest
       const nextBtn = index === 0 ? 1 : 0;
       // Del que esta remueve la clase
-      list[nextBtn].classList.remove("active");
+      list[nextBtn].classList.remove("active", ...borderClasses);
       // añadesela al btn sobre el que se está clickando
-      btn.classList.add("active");
+      btn.classList.add("active", ...borderClasses);
 
       const controls = btn.attributes["aria-controls"].value;
       const elementDiv = document.getElementById(controls);
